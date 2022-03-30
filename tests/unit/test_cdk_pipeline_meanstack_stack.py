835aad4da31e7470961796184594575379622b2f
Ever <everestuar@gmail.com>
2022-03-30 00:03:53 -0600

    Initial commit

---
+++ tests/unit/test_cdk_pipeline_meanstack_stack.py
@@ -0,0 +1,19 @@
+# from aws_cdk import (
+#         core,
+#         assertions
+#     )
+
+# from cdk_pipeline_meanstack.cdk_pipeline_meanstack_stack import CdkPipelineMeanstackStack
+
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in cdk_pipeline_meanstack/cdk_pipeline_meanstack_stack.py
+def test_sqs_queue_created():
+#     app = core.App()
+#     stack = CdkPipelineMeanstackStack(app, "cdk-pipeline-meanstack")
+#     template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
+  pass
