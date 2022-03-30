e6627357e61c7a888eddb900d27cbc2bd85eb935
Ever <everestuar@gmail.com>
2022-03-30 10:50:37 -0600

    pipeline integrates into existing components

--- app.py
+++ app.py
@@ -1,19 +1,37 @@
 #!/usr/bin/env python3
 import os
-
+import distutils.util
 from aws_cdk import core as cdk
-
-# For consistency with TypeScript code, `cdk` is the preferred import name for
-# the CDK's core module.  The following line also imports it as `core` for use
-# with examples from the CDK Developer's Guide, which are in the process of
-# being updated to use `cdk`.  You may delete this import if you don't need it.
 from aws_cdk import core
-
 from cdk_pipeline_meanstack.cdk_pipeline_meanstack_stack import CdkPipelineMeanstackStack
 
+## START of Parameters Definition
+parms = {}
+
+parms["stage"] = os.getenv("STAGE", "dev")
+parms["vpc_id"] = os.getenv("VPC_ID", "")
+parms["vpc_default"] = bool(distutils.util.strtobool(os.getenv("VPC_DEFAULT", 'False')))
+parms["sg_id"] = os.getenv("SG_ID", "")
+parms["repo_owner"] = os.getenv("REPO_OWNER", "everestuar")
+parms["repo_name"] = os.getenv("REPO_NAME", "")
+parms["repo_branch"] = os.getenv("REPO_BRANCH", "")
+parms["ecr_repo_name"] = os.getenv("ECR_REPO_NAME")
+parms["ecr_repo_uri"] = os.getenv("ECR_REPO_URI")
+parms["ecr_repo_arn"] = os.getenv("ECR_REPO_ARN")
+parms["ecs_cluster_name"] = os.getenv("ECS_CLUSTER_NAME")
+parms["ecs_fargate_service"] = os.getenv("ECS_FARGET_SERVICE")
+parms["pipeline_name"] = os.getenv("PIPELINE_NAME")
+parms["dockerhub_username"] = os.getenv("DOCKERHUB_USERNAME")
+parms["dockerhub_password"] = os.getenv("DOCKERHUB_PASSWORD")
+parms["github_token"] = os.getenv("GITHUB_TOKEN")
+
+## END of Parameters Definition
+
+env_NV = cdk.Environment(account=os.environ.get("DEPLOY_ACCOUNT", os.environ["DEFAULT_ACCOUNT"]),
+    region=os.environ.get("DEPLOY_REGION", os.environ["DEFAULT_REGION"]))
 
 app = core.App()
-CdkPipelineMeanstackStack(app, "CdkPipelineMeanstackStack",
+    # CdkPipelineMeanstackStack(app, "CdkPipelineMeanstackStack",
     # If you don't specify 'env', this stack will be environment-agnostic.
     # Account/Region-dependent features and context lookups will not work,
     # but a single synthesized template can be deployed anywhere.
@@ -29,6 +47,9 @@ CdkPipelineMeanstackStack(app, "CdkPipelineMeanstackStack",
     #env=core.Environment(account='123456789012', region='us-east-1'),
 
     # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
+    #     )
+
+pipeline = CdkPipelineMeanstackStack(app, "Pipeline-"+parms["stage"], env=env_NV, parms=parms)
+core.Tags.of(pipeline).add("Env", parms["stage"])
 
 app.synth()
--- cdk_pipeline_meanstack/cdk_pipeline_meanstack_stack.py
+++ cdk_pipeline_meanstack/cdk_pipeline_meanstack_stack.py
@@ -2,23 +2,162 @@ from aws_cdk import (
     core as cdk
     # aws_sqs as sqs,
 )
-
-# For consistency with other languages, `cdk` is the preferred import name for
-# the CDK's core module.  The following line also imports it as `core` for use
-# with examples from the CDK Developer's Guide, which are in the process of
-# being updated to use `cdk`.  You may delete this import if you don't need it.
 from aws_cdk import core
+import aws_cdk.aws_ec2 as ec2
+import aws_cdk.aws_codebuild as codebuild
+import aws_cdk.aws_codepipeline as codepipeline
+import aws_cdk.aws_codepipeline_actions as codepipeline_actions
+import aws_cdk.aws_iam as iam
+import aws_cdk.aws_ecs as ecs
 
 
 class CdkPipelineMeanstackStack(cdk.Stack):
 
-    def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: cdk.Construct, construct_id: str, parms=dict, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # Reference to existing ECS Cluster and its configuration
+        
+        my_vpc = ec2.Vpc.from_lookup(self, "vpc"+parms['stage'],vpc_id = parms['vpc_id'], is_default = parms["vpc_default"])
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkPipelineMeanstackQueue",
-        #     visibility_timeout=cdk.Duration.seconds(300),
-        # )
+        my_security_group = ec2.SecurityGroup.from_security_group_id(self, parms["sg_id"]+parms["stage"], parms["sg_id"])
+
+        ecs_cluster = ecs.Cluster.from_cluster_attributes(self, parms["ecs_cluster_name"],
+            cluster_name=parms["ecs_cluster_name"],
+            security_groups=[my_security_group],
+            vpc=my_vpc,
+        )
+
+        ecs_service = ecs.FargateService.from_fargate_service_attributes(self, parms["ecs_fargate_service"],
+            cluster=ecs_cluster,
+            service_name=parms["ecs_fargate_service"]
+        )        
+
+        # Github Repo
+        gitHubSource = codebuild.Source.git_hub(
+            owner=parms["repo_owner"],
+            repo=parms["repo_name"],
+            branch_or_ref=parms["repo_branch"],
+            clone_depth=1,
+            webhook=False,                        
+        )
+
+        # Buildspec for codebuild project
+        buildspec = {
+            "version": "0.2",
+            "phases": {
+                "pre_build": {
+                    "commands": [
+                        'env',
+                        'aws --version',
+                        'docker login -u $dockerhub_username -p $dockerhub_password',
+                        'COMMIT_HASH=$(echo $CODEBUILD_RESOLVED_SOURCE_VERSION | cut -c 1-7)',
+                        'IMAGE_TAG=${COMMIT_HASH:=latest}',
+                        'env'
+                    ]
+                },
+                "build": {                
+                    "commands": [
+                        'echo "In Build Stage"', 
+                        'echo Build started on `date`',
+                        'docker build . -t $ECR_REPO_URI:latest',
+                        'docker tag $ECR_REPO_URI:latest $ECR_REPO_URI:$IMAGE_TAG',
+                        'echo Build completed on `date`'
+                    ]
+                },
+                "post_build": {
+                    "commands": [
+                        'echo "In Post-Build Stage"',
+                        'echo Post-Build started on `date`',
+                        'docker login -u AWS -p $(aws ecr get-login-password --region $AWS_REGION) $ECR_REPO_URI',
+                        'docker push $ECR_REPO_URI:latest',
+                        'docker push $ECR_REPO_URI:$IMAGE_TAG',
+                        "printf '[{\"name\":\"mean-stack-front-end\", \"imageUri\":\"%s:%s\"}]' $ECR_REPO_URI $IMAGE_TAG > imagedefinitions.json",
+                        "pwd; ls -al; cat imagedefinitions.json",
+                        "echo Post-Build completed on `date`"
+                    ]
+                }
+            },
+            "artifacts" : {
+                "files": [
+                    'Angular6/imagedefinitions.json'
+                ]
+            }
+        }
+
+        # Codebuild Project
+        build_project = codebuild.Project(self, parms["ecs_cluster_name"] + parms["stage"] + "-bp",
+            project_name=parms["ecs_cluster_name"]+parms["stage"]+"bp",
+            source=gitHubSource,
+            environment=codebuild.BuildEnvironment(
+                build_image=codebuild.LinuxBuildImage.STANDARD_5_0,
+                privileged=True
+            ),
+            environment_variables=(
+                {
+                    'ECR_REPO_URI': codebuild.BuildEnvironmentVariable(value=parms["ecr_repo_uri"]),
+                    'ECS_CLUSTER_NAME': codebuild.BuildEnvironmentVariable(value=parms["ecs_cluster_name"]),
+                    'dockerhub_username': codebuild.BuildEnvironmentVariable(value=parms["dockerhub_username"], 
+                        type=codebuild.BuildEnvironmentVariableType(value='SECRETS_MANAGER')),
+                    'dockerhub_password': codebuild.BuildEnvironmentVariable(value=parms["dockerhub_password"], 
+                        type=codebuild.BuildEnvironmentVariableType(value='SECRETS_MANAGER'))
+                }
+            ),
+            # build_spec=codebuild.BuildSpec.from_object(buildspec)
+            build_spec=codebuild.BuildSpec.from_source_filename('Angular6/buildspec.yml')            
+        )
+
+        # Codepipeline Actions
+        source_output = codepipeline.Artifact()
+        build_output = codepipeline.Artifact()
+
+        source_action = codepipeline_actions.GitHubSourceAction(
+            action_name='GitHub_Source',
+            owner=parms["repo_owner"],
+            repo=parms["repo_name"],
+            branch=parms["repo_branch"],
+            oauth_token=cdk.SecretValue.secrets_manager(parms["github_token"]),
+            output=source_output
+        )
+
+        build_action = codepipeline_actions.CodeBuildAction(
+            action_name='CodeBuild',
+            project=build_project,
+            input=source_output,
+            outputs=[build_output]
+        )
+
+        deploy_action = codepipeline_actions.EcsDeployAction(
+            action_name='DeployAction',
+            service=ecs_service,
+            image_file=codepipeline.ArtifactPath(build_output, 'Angular6/imagedefinitions.json')
+        )
+
+        # Codepipeline
+        pipeline = codepipeline.Pipeline(self, "CodePipeline",
+            pipeline_name=parms["pipeline_name"],
+            stages=[
+                codepipeline.StageProps(stage_name="Source", actions=[source_action]),
+                codepipeline.StageProps(stage_name="Build", actions=[build_action]),
+                codepipeline.StageProps(stage_name="Deploy-to-ECS", actions=[deploy_action])
+            ]
+        )
+
+        # IAM extra policies to codebuild
+        build_project.add_to_role_policy(iam.PolicyStatement(
+                actions=["codestar-connections:UseConnection"],
+                resources=["*"]
+            )
+        )
+
+        build_project.add_to_role_policy(iam.PolicyStatement(
+                actions=["ecr:*", "ecs:*"],
+                resources=["*"]
+            )
+        )
+
+        build_project.add_to_role_policy(iam.PolicyStatement(
+                actions=["secretsmanager:GetSecretValue"],
+                resources=["*"]
+            )
+        )
